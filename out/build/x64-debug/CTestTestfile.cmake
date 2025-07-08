91fe654806897ea695250b2fed650be864ca83c8
PAVITHRA3582 <PAVITHRA121CHENNAVEERA@GMAIL.COM>
2025-07-08 12:10:02 +0530

    updated

---
+++ out/build/x64-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrator/Desktop/MyFirstCICD
+# Build directory: C:/Users/Administrator/Desktop/MyFirstCICD/out/build/x64-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_main]=] "C:/Users/Administrator/Desktop/MyFirstCICD/out/build/x64-debug/test_main.exe")
+set_tests_properties([=[test_main]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/MyFirstCICD/CMakeLists.txt;29;add_test;C:/Users/Administrator/Desktop/MyFirstCICD/CMakeLists.txt;33;add_calc_test;C:/Users/Administrator/Desktop/MyFirstCICD/CMakeLists.txt;0;")
+add_test([=[test_main2]=] "C:/Users/Administrator/Desktop/MyFirstCICD/out/build/x64-debug/test_main2.exe")
+set_tests_properties([=[test_main2]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/MyFirstCICD/CMakeLists.txt;29;add_test;C:/Users/Administrator/Desktop/MyFirstCICD/CMakeLists.txt;34;add_calc_test;C:/Users/Administrator/Desktop/MyFirstCICD/CMakeLists.txt;0;")
